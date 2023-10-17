cb7d96a321836fada934d01d54413b25f3d721cc
Padrito <124273198+Padrito@users.noreply.github.com>
2023-10-17 21:39:05 +0400

    Add files via upload
    დავალება 5

---
+++ davaleba 5/davaleba5.js
@@ -0,0 +1,43 @@
+
+//davaleba 1
+// const bodyEl = document.body    
+
+// const divEl = document.createElement("div")
+// const text = document.createTextNode("Magic Text")
+// divEl.appendChild(text)
+// document.body.appendChild(divEl)
+
+
+// const btn = document.getElementById("coolButton")
+// btn.onclick = function(){
+//     divEl.remove()
+// }
+
+//davaleba 2
+// const body = document.querySelector('body')
+// const div = document.createElement('div')
+// div.setAttribute("id", "card")
+// const header = document.createElement('h1')
+// const anchor = document.createElement('a')
+// anchor.setAttribute("href", "#")
+// const gandalf = document.createTextNode("Gandalf")
+// const profile = document.createTextNode("Go to Profile")
+
+
+// header.appendChild(gandalf)
+// anchor.appendChild(profile)
+
+// body.appendChild(div)
+// div.append(header, anchor)
+
+//davaleba3
+
+
+// const wrong = document.querySelector(".wrong1")
+// const right = document.querySelectorAll(".right")
+// console.log(wrong)
+
+// wrong.addEventListener("click", (e) => {
+//     wrong.style.color = "red"
+// })
+
